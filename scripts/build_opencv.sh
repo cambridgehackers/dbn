1423945f622d44d795665db2c038b2ef475590bb
Jamey Hicks <jamey.hicks@qrclab.com>
2014-04-23 11:48:53 -0400

    make after running cmake

--- scripts/build_opencv.sh
+++ scripts/build_opencv.sh
@@ -6,3 +6,4 @@ OPENCV_DIR=$PWD
 mkdir build; cd build
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$OPENCV_DIR/platforms/android/android.toolchain.cmake -DANDROID_STL=stlport_static -DANDROID_NATIVE_API_LEVEL=19 ..
+make -j8
